0cd3f5fa5ff893062a0c00c2cc30a9576b6edbfe
Jasbir Singh <sjasdaman123@gmail.com>
2026-05-13 22:56:12 +0530

    fixed live api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,24 +1,21 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+
+  baseURL: import.meta.env.VITE_API_URL,
+
 });
 
-// Request interceptor to add the auth token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token && token !== 'undefined' && token !== 'null') {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 export default api;
